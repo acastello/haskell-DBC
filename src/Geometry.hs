be44380764fd6b61cffb49ebecbe06e79fabe825
ACastello <a.castello.pas@gmail.com>
2017-03-28 21:30:29 +0200

    .

--- src/Geometry.hs
+++ src/Geometry.hs
@@ -1,8 +1,28 @@
 module Geomtry where
 
+import Data.List
+import Data.Maybe
+import qualified Data.List as L
+
 import Types
 
 type Polygon = [Point]
 
 contained :: Point -> Polygon -> Bool
-contained p pol
+contained _ [] = False
+contained p (head:pol) = length left `mod` 2 /= 0 || length right `mod` 2 /= 0
+  where
+    (left, right) = partition (< p_x p) crosses
+    crosses = fmap (uncurry hei) edges
+    edges = catMaybes $ snd $ mapAccumR f head pol
+    f p' p'' =
+        if signum (p_y p - p_y p') /= signum (p_y p - p_y p') then
+            (p'', Just (p',p''))
+        else
+            (p'', Nothing)
+    hei :: Point -> Point -> Float
+    hei p' p'' = m * (p_y p) + n where
+        m = (p_x p'' - p_x p') / (p_y p'' - p_y p')
+        n = p_y p' - m * p_x p'
+
+
